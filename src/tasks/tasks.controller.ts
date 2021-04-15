54882a151568bfe156c1296d0ed432cfaa04d118
gaiyadev <gaiyaobed94@gmail.com>
2021-04-15 16:51:18 +0100

    task done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,31 +3,31 @@ import {
   Controller,
   Delete,
   Get,
-  Param, ParseIntPipe,
+  Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
   UsePipes,
-  ValidationPipe
-} from "@nestjs/common";
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { TasksService } from './tasks.service';
-import { Task } from "./task.entity";
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length) {
-  //     this.taskService.getTaskWithFilter(filterDto);
-  //   } else {
-  //     return this.taskService.getAllTask();
-  //   }
-  // }
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTaskFilterDto,
+  ): Promise<Task[]> {
+    return this.taskService.getTasks(filterDto);
+  }
 
   @Post()
   @UsePipes(ValidationPipe)
@@ -45,12 +45,12 @@ export class TasksController {
   deleteTaskById(@Param('id', ParseIntPipe) id: number): Promise<any> {
     return this.taskService.deleteTaskById(id);
   }
-
-  // @Patch('/:id/status')
-  // updateTaskById(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  // ): Task {
-  //   return this.taskService.updateTaskById(id, status);
-  // }
+  //
+  @Patch('/:id/status')
+  updateTaskStatusById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.taskService.updateTaskStatusById(id, status);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,35 +4,27 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskRepository } from './task-repository';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
-import Any = jasmine.Any;
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(TaskRepository) private taskRepository: TaskRepository,
   ) {}
-  // getAllTask(): Task[] {
-  //   return this.tasks;
-  // }
-  // getTaskWithFilter(filterDto: GetTaskFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTask();
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       (task) =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-  //   return tasks;
-  // }
+
+  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<any> {
     const task = await this.taskRepository.createTask(createTaskDto);
     return {
-      task: { id: task.id, title: task.title, description: task.description },
+      task: {
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        status: task.status,
+      },
       message: 'Task created successfully',
     };
   }
@@ -53,9 +45,10 @@ export class TasksService {
     return { message: 'Task deleted successfully' };
   }
 
-  // updateTaskById(id: string, status: TaskStatus): Task {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
+  async updateTaskStatusById(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await task.save();
+    return task;
+  }
 }
