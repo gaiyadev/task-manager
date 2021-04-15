25f102b158ef04f25f323f5afe51f57e0577a59a
gaiyadev <gaiyaobed94@gmail.com>
2021-04-15 11:55:07 +0100

    Db Connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
